43a15f855bfeefcfa67524df303c131b338645c9
Kamirii <camilyalbres@gmail.com>
2022-08-10 16:15:23 -0300

    criando conexão LivrosController

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import bd from './data/sqlite.js';
+import {Livros} from './controller/LivrosController.js'
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+Livros(app,bd)
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
