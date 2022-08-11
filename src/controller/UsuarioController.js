cbedd10ab03b95296c357997d6126240bbbc9c1f
Lucasmedeiros7 <lucaasmedeiros7@gmail.com>
2022-08-11 19:17:11 -0300

    Utilizando async e await no UsuarioController

--- src/controller/UsuarioController.js
+++ src/controller/UsuarioController.js
@@ -1,82 +1,86 @@
-import UsuarioDAO from "../DAO/UsuarioDAO.js"
-import { validacaoUsuario } from "../middleware/validacaoUsuario.js"
+import UsuarioDAO from '../DAO/UsuarioDAO.js';
+import { validacaoUsuario } from '../middleware/validacaoUsuario.js';
+import UsuarioModel from '../model/UsuarioModel.js';
 
 export const Usuarios = (app, bd) => {
+  //CREATE
+  app.post('/usuarios', validacaoUsuario, async (req, res) => {
+    const { nome, email, senha, CPF } = req.body;
+    const usuario = new UsuarioModel(nome, email, senha, CPF);
 
-    //CREATE
+    try {
+      await UsuarioDAO.adicionarUsuario(bd, usuario);
+      res.status(201).send({ message: 'Usuário adicionado com sucesso!' });
+    } catch (erro) {
+      res.status(400).json({ error: erro.message });
+    }
+  });
 
-    app.post('/usuarios', validacaoUsuario, (req, res) => {
-        const usuario = req.body
-        UsuarioDAO.adicionarUsuario(bd, usuario)
-        try {
-            res.status(200).send('Usuário adicionado com sucesso!')
-        }
-        catch (erro) {
-            res.status(500).json(erro)
-        }
-    })
+  //READ
+  app.get('/usuarios', async (req, res) => {
+    try {
+      const usuarios = await UsuarioDAO.listarUsuarios(bd);
 
-    //READ
+      if (!usuarios) {
+        return res.status(404).json('Nenhum usuário cadastrado!');
+      }
 
-    app.get('/usuarios', (req, res) => {
-        UsuarioDAO.listarUsuarios(bd)
-        try {
-            success.senha = undefined
-            res.status(200)
-        }
-        catch (erro) {
-            res.status(500).json(erro)
-        }
-    })
+      res.status(200).json({ usuarios });
+    } catch (erro) {
+      res.status(400).json({ error: erro.message });
+    }
+  });
 
-    app.get('/usuarios/:id', (req, res) => {
-        const id = req.params.id
-        UsuarioDAO.listarUsuariosPorId(bd, id)
-        try {
-            success.senha = undefined
-            res.status(200)
-        }
-        catch (erro) {
-            res.status(500).json(erro)
-        }
-    })
+  app.get('/usuarios/:id', async (req, res) => {
+    const id = req.params.id;
 
-    //UPDATE 
+    try {
+      const usuario = await UsuarioDAO.listarUsuariosPorId(bd, id);
 
-    app.patch('/usuarios/:id', async (req, res) => {
-        const id = req.params.id
-        const usuarioAtualizado = req.body
-        const usuarioBd = UsuarioDAO.listarUsuariosPorId(bd, id)
+      if (!usuario) {
+        return res.status(404).json({ message: 'Usuário não existe!' });
+      }
 
-        if (usuarioBd === undefined) {
-            res.status(404).send('O usuário especificado não foi encontrado.')
-        }
+      res.status(200).json(usuario);
+    } catch (erro) {
+      res.status(500).json({ error: erro.message });
+    }
+  });
 
-        try {
-            await UsuarioDAO.atualizarUsuario(bd, id, usuarioAtualizado)
-            res.status(200).send('Usuário atualizado com sucesso.')
-        }
-        catch (error) {
-            res.status(500).json(error)
-        }
-    })
+  //UPDATE
+  app.patch('/usuarios/:id', async (req, res) => {
+    const id = req.params.id;
+    const { nome, email, senha, CPF } = req.body;
+    const usuarioAtualizado = new UsuarioModel(nome, email, senha, CPF);
 
-    //DELETE
+    const usuarioBd = await UsuarioDAO.listarUsuariosPorId(bd, id);
 
-    app.delete('/usuarios/:id', async (req, res) => {
-        const id = req.params.id
-        const usuarioBd = UsuarioDAO.listarUsuariosPorId(bd, id)
+    if (!usuarioBd) {
+      return res.status(404).json({ message: 'O usuário especificado não foi encontrado.' });
+    }
 
-        if (usuarioBd === undefined) {
-            res.status(404).send('O usuário especificado não foi encontrado.')
-        }
+    try {
+      await UsuarioDAO.atualizarUsuario(bd, id, usuarioAtualizado);
+      res.status(200).json({ message: 'Usuário atualizado com sucesso.' });
+    } catch (error) {
+      res.status(400).json({ error: erro.message });
+    }
+  });
 
-        try {
-            await UsuarioDAO.deletarUsuario(bd, id)
-            res.status(200).send('Usuário deletado com sucesso.')
-        }
-        catch (erro) {
-            res.status(500).json(erro)
-        }
-    })
-}
+  //DELETE
+  app.delete('/usuarios/:id', async (req, res) => {
+    const id = req.params.id;
+    const usuarioBd = await UsuarioDAO.listarUsuariosPorId(bd, id);
+
+    if (!usuarioBd) {
+      return res.status(404).json({ message: 'O usuário especificado não foi encontrado.' });
+    }
+
+    try {
+      await UsuarioDAO.deletarUsuario(bd, id);
+      res.status(200).json({ message: 'Usuário deletado com sucesso.' });
+    } catch (erro) {
+      res.status(400).json({ error: erro.message });
+    }
+  });
+};
