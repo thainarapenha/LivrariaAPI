b7a6e17a89e842464f45d7cb09f9dc223e2e1283
Isadora Áraújo <isadoraraujo13@gmail.com>
2022-08-12 11:35:31 -0300

    correção da indentação do models de funcionários

--- src/DAO/FuncionariosDAO.js
+++ src/DAO/FuncionariosDAO.js
@@ -1,3 +1,11 @@
 export default class FuncionariosDAO {
-    
+    static listarFuncionarios(bd) {
+        return new Promisse = ((res, rej) => {
+            bd.all('SELECT * FROM funcionarios', (erro, linhas) => {
+                if(erro) {
+                    console.log(erro)
+                }
+            })
+        })
+    }
 }
