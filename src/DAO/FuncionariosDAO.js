985b465c73a30dd535d050578bf4f3a87a56dd3d
Isadora Áraújo <isadoraraujo13@gmail.com>
2022-08-12 11:36:57 -0300

    read criado no DAO

--- src/DAO/FuncionariosDAO.js
+++ src/DAO/FuncionariosDAO.js
@@ -2,8 +2,12 @@ export default class FuncionariosDAO {
     static listarFuncionarios(bd) {
         return new Promisse = ((res, rej) => {
             bd.all('SELECT * FROM funcionarios', (erro, linhas) => {
-                if(erro) {
+                if (erro) {
                     console.log(erro)
+                    rej(erro)
+                }
+                else {
+                    res(linhas)
                 }
             })
         })
