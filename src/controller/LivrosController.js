632b792db46deaf1d114d7bdbe0c2ef59daa0a4c
Kamirii <camilyalbres@gmail.com>
2022-08-12 17:51:08 -0300

    correção chamada de função na rota get livros/id

--- src/controller/LivrosController.js
+++ src/controller/LivrosController.js
@@ -1,41 +1,42 @@
 import LivroDAO from "../DAO/LivrosDAO.js"
 import bd from "../data/sqlite.js"
+import LivrosModel from "../model/LivrosModel.js"
 
-export const Livros = app => { 
+export const Livros = app => {
 
     //READ 
-    app.get('/livros/', (req, res) => { 
+    app.get('/livros/', (req, res) => {
         LivroDAO.listarLivros(bd)
-        .then((success) => { 
-            res.status(200).json(success)
-        }).catch((error) => { 
-            res.status(400).json(error)
-        })
-
-    })
-
-    app.get('/livros/:id', (req, res) => { 
-        const id_livro = req.params.id
-        LivroDAO.listarLivros(bd, id_livro)
-        .then((success) => { 
-            res.status(200).json(success)
-        }).catch((error) => { 
-            res.status(400).json(error)
-        })
-    })
-    
-     //CREATE 
-     app.post('/livros', (req, res) => {
-        const livro = req.body
-        LivroDAO.adicionarLivros(bd,livro)
             .then((success) => {
                 res.status(200).json(success)
+            }).catch((error) => {
+                res.status(400).json(error)
             })
-            .catch((erro) => {
-                res.status(400).json(erro)
-                
+
+    })
+
+    app.get('/livros/:id', (req, res) => {
+        const id_livro = req.params.id
+        LivroDAO.listarLivrosPorId(bd, id_livro)
+            .then((success) => {
+                res.status(200).json(success)
+            }).catch((error) => {
+                res.status(400).json(error)
             })
     })
 
+    //CREATE 
+    app.post('/livros', async (req, res) => {
+        const { titulo, descricao, categoria, url_img, preco, total_paginas, ano_publicacao, autor } = req.body
+        const livro = new LivrosModel(titulo, descricao, categoria, url_img, preco, total_paginas, ano_publicacao, autor)
+        try {
+            await LivroDAO.adicionarLivros(bd, livro)
+            res.status(200).json(success)
+        } catch (erro) {
+            res.status(400).json(erro)
+
+        }
+    })
+    //UPDATE 
 
 }
