262f77a4fc75a3814baac6d0d4f5ee831a0e3bb0
Isadora Áraújo <isadoraraujo13@gmail.com>
2022-08-12 10:57:57 -0300

    models de funcionários criado

---
+++ src/model/FuncionariosModel.js
@@ -0,0 +1,10 @@
+export default class FuncionariosModel {
+    constructor(id, CPF, nome, cargo, salario, status) {
+        this.id = id,
+        this.cpf = CPF,
+        this.nome =  nome,
+        this.cargo = cargo,
+        this.salario = salario, 
+        this.status = status
+    }
+}
