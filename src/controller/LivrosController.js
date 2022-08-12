3877d13de17dcc4282288b4e9150d927c4fef679
Kamirii <camilyalbres@gmail.com>
2022-08-12 17:52:15 -0300

    correção rota post create e criação da rota update

--- src/controller/LivrosController.js
+++ src/controller/LivrosController.js
@@ -28,15 +28,45 @@ export const Livros = app => {
     //CREATE 
     app.post('/livros', async (req, res) => {
         const { titulo, descricao, categoria, url_img, preco, total_paginas, ano_publicacao, autor } = req.body
-        const livro = new LivrosModel(titulo, descricao, categoria, url_img, preco, total_paginas, ano_publicacao, autor)
+        const novoLivro = new LivrosModel(titulo, descricao, categoria, url_img, preco, total_paginas, ano_publicacao, autor)
+        const message = {
+            success: 'Livro cadastrado com sucesso',
+            error: false
+        }
         try {
-            await LivroDAO.adicionarLivros(bd, livro)
-            res.status(200).json(success)
+            await LivroDAO.adicionarLivros(bd, novoLivro)
+            res.status(201).json(message.success)
         } catch (erro) {
-            res.status(400).json(erro)
+            res.status(400).json(message.error)
 
         }
     })
     //UPDATE 
+    app.patch('/livros/:id', async (req,res) => { 
+        const id_livro = req.params.id
+        const { titulo, descricao, categoria, url_img, preco, total_paginas, ano_publicacao, autor } = req.body
+        const livroAtualizado = new LivrosModel(titulo, descricao, categoria, url_img, preco, total_paginas, ano_publicacao, autor)
+        const message = {
+            success: 'O livro especificado não foi encontrado.',
+            error: false
+          }
+          const messageAtt = {
+            success: 'Livro atualizado com sucesso.',
+            error: false
+          }
+          const livroExiste = await LivroDAO.listarLivrosPorId(bd, id_livro);
+          if (!livroExiste) {
+            return res
+              .status(404)
+              .json(message.success);
+          }
+          try {
+            await LivroDAO.atualizarLivro(bd, id_livro, livroAtualizado); // parametros chamados
+            res.status(200).json(messageAtt.success);
+          } catch (error) {
+            res.status(400).json({ error: erro.message });
+          }
+        });
+      
 
 }
