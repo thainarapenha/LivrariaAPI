9ec73615619544fb5b14a11a11949f3e3f4935fe
Thainara <thainarapenha@alu.ufc.br>
2022-08-14 16:33:48 -0300

    Criando os demais métodos do CRUD no arquivo EstoqueController

--- src/controller/EstoqueController.js
+++ src/controller/EstoqueController.js
@@ -23,9 +23,59 @@ export const Estoque = (app, bd) => {
         }
         try {
             await EstoqueDAO.adicionarEstoque(bd, estoque)
-            res.status(201).json(message.success)
+            res.status(200).json(message.success)
         } catch (erro) {
             res.status(400).json({error: erro.message })
+        }
+    });
+
+    //UPDATE 
+    app.patch('/estoque/:id', async(req, res) => {
+        const id_estoque = req.params.id
+        const {nome_fornecedor, CNPJ, qnt_livros, lote, nome_obra, preco_lote} = req.body;
+        const estoqueAtualizado = new EstoqueModel(nome_fornecedor, CNPJ, qnt_livros, lote, nome_obra, preco_lote)
+        const message = {
+            success: 'O estoque especificado não foi encontrado.',
+            error: false
+        }
+        const messageAtt = {
+            success: 'Estoque atualizado com sucesso.',
+            error: false
+        }
+        const EstoqueExiste = await EstoqueDAO.listarEstoquePorId(bd, id_estoque);
+            if(!EstoqueExiste){
+                return res
+                    .status(404)
+                    .json(message.success);
+            }
+            try {
+                await EstoqueDAO.atualizarEstoque(bd, id_estoque, estoqueAtualizado); // parametros chamados
+                res.status(200).json(messageAtt.success);
+              } catch (erro) {
+                res.status(400).json({ error: erro.message });
+              }
+    });
+
+    //DELETE
+    app.delete('/estoque/:id', async(req, res) => {
+        const id_estoque = req.params.id
+        const EstoqueExiste = await EstoqueDAO.listarEstoquePorId(bd, id_estoque);
+        const message = {
+            success: 'O estoque especificado não foi encontrado.',
+            error: false
+        }
+        const messageDelete = {
+            success: 'Estoque deletado com sucesso.',
+            error: false
+        }
+        if(!EstoqueExiste){
+            return res.status(404).json(message.success)
+        }
+        try{ 
+            await EstoqueDAO.deletarEstoque(bd, id_estoque)
+            res.status(200).json(messageDelete.sucess)
+        }catch(erro){
+            res.status(400).json({error: erro.message });
         }
     });
 }
