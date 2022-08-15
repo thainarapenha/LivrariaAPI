32653fd35718f13be6ee1d3a4e3432f5bfd68f5e
Thainara Penha <43799802+thainarapenha@users.noreply.github.com>
2022-08-15 14:40:45 -0300

    resolvendo novos conflitos de código de branch

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import bd from './data/sqlite.js';
 
+
+
 import {Livros} from './controller/LivrosController.js'
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+
 
 
 Livros(app,bd)
