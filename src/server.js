59628a67ebf2295e6669d84b833a58c751800045
Thainara <thainarapenha@alu.ufc.br>
2022-08-15 14:51:05 -0300

    ajustando arquivo server para solicitação de PR

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import bd from './data/sqlite.js';
 
+
+
+import { Estoque } from './controller/EstoqueController.js';
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+
+
+
+Estoque.use(app, bd);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
