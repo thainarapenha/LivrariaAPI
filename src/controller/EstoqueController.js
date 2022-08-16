e5f0ee7590412e5d3ba0b5050789c0ec77b44e69
Thainara <thainarapenha@alu.ufc.br>
2022-08-16 16:28:12 -0300

    corrigindo rotas de estoque

--- src/controller/EstoqueController.js
+++ src/controller/EstoqueController.js
@@ -1,92 +1,95 @@
-import EstoqueDAO from "../DAO/EstoqueDAO.js";
-import EstoqueModel from "../model/EstoqueModel.js"
+import EstoqueDAO from '../DAO/EstoqueDAO.js';
+import EstoqueModel from '../model/EstoqueModel.js';
 
 export const Estoque = (app, bd) => {
-
     //READ
-    app.get('/estoque', (req, res) => {
-        EstoqueDAO.listarEstoque(bd)
-            .then((success) => {
-                res.status(200).json(success)
-            }).catch((error) => {
-                res.status(400).json(error)
-            })
-    })
-
-    //READ for Id
-    app.get('/estoque/:id', (req, res) => {
-        const id_estoque = req.params.id
-        EstoqueDAO.listarEstoquePorId(bd, id_estoque)
-            .then((success) => {
-                res.status(200).json(success)
-            }).catch((error) => {
-                res.status(400).json(error)
-            })
-    })
-
-    //CREATE
-    app.post('/estoque', async (req, res) => {
-        const { nome_fornecedor, CNPJ, qnt_livros, lote, nome_obra, preco_lote } = req.body;
-        const estoque = new EstoqueModel(nome_fornecedor, CNPJ, qnt_livros, lote, nome_obra, preco_lote)
-        const message = {
-            success: 'Estoque cadastrado com sucesso',
-            error: false
-        }
+    app.get('/estoque', async (req, res) => {
         try {
-            await EstoqueDAO.adicionarEstoque(bd, estoque)
-            res.status(200).json(message.success)
-        } catch (erro) {
-            res.status(400).json({ error: erro.message })
+            const estoque = await EstoqueDAO.listarEstoque(bd);
+            res.status(200).json(estoque);
+        } catch (err) {
+            res.status(400).json(err);
         }
     });
 
-    //UPDATE 
-    app.patch('/estoque/:id', async (req, res) => {
-        const id_estoque = req.params.id
-        const { nome_fornecedor, CNPJ, qnt_livros, lote, nome_obra, preco_lote } = req.body;
-        const estoqueAtualizado = new EstoqueModel(nome_fornecedor, CNPJ, qnt_livros, lote, nome_obra, preco_lote)
-        const message = {
-            success: 'O estoque especificado não foi encontrado.',
-            error: false
-        }
-        const messageAtt = {
-            success: 'Estoque atualizado com sucesso.',
-            error: false
-        }
-        const EstoqueExiste = await EstoqueDAO.listarEstoquePorId(bd, id_estoque);
-        if (!EstoqueExiste) {
-            return res
-                .status(404)
-                .json(message.success);
-        }
+    //READ for Id
+    app.get('/estoque/:id', async (req, res) => {
         try {
-            await EstoqueDAO.atualizarEstoque(bd, id_estoque, estoqueAtualizado); // parametros chamados
-            res.status(200).json(messageAtt.success);
+            const id_estoque = req.params.id;
+            const estoque = await EstoqueDAO.listarEstoquePorId(bd, id_estoque);
+
+            if (!estoque) {
+                return res.status(400).json({ message: 'Não encontrado' });
+            }
+            res.status(200).json(estoque);
+        } catch (err) {
+            res.status(400).json(err);
+        }
+    });
+
+    //CREATE
+    app.post('/estoque', async (req, res) => {
+        const { nome_fornecedor, CNPJ, qnt_livros, lote, nome_obra, preco_lote } =
+            req.body;
+        const estoque = new EstoqueModel(
+            nome_fornecedor,
+            CNPJ,
+            qnt_livros,
+            lote,
+            nome_obra,
+            preco_lote
+        );
+
+        try {
+            await EstoqueDAO.adicionarEstoque(bd, estoque);
+            res.status(200).json({ message: 'Adicionado ao estoque com sucesso' });
         } catch (erro) {
-            res.status(400).json({ error: erro.message });
+            res.status(400).json({ error: erro });
+        }
+    });
+
+    //UPDATE
+    app.patch('/estoque/:id', async (req, res) => {
+        const id_estoque = req.params.id;
+        const { nome_fornecedor, CNPJ, qnt_livros, lote, nome_obra, preco_lote } =
+            req.body;
+        const estoqueAtualizado = new EstoqueModel(
+            nome_fornecedor,
+            CNPJ,
+            qnt_livros,
+            lote,
+            nome_obra,
+            preco_lote
+        );
+
+        const estoqueExiste = await EstoqueDAO.listarEstoquePorId(bd, id_estoque);
+
+        if (!estoqueExiste) {
+            return res.status(404).json({ message: 'Não encontrado' });
+        }
+
+        try {
+            await EstoqueDAO.atualizarEstoque(bd, id_estoque, estoqueAtualizado);
+            res.status(200).json({ message: 'Atualizado com sucesso' });
+        } catch (erro) {
+            res.status(400).json({ error: erro });
         }
     });
 
     //DELETE
     app.delete('/estoque/:id', async (req, res) => {
-        const id_estoque = req.params.id
-        const EstoqueExiste = await EstoqueDAO.listarEstoquePorId(bd, id_estoque);
-        const message = {
-            success: 'O estoque especificado não foi encontrado.',
-            error: false
+        const id_estoque = req.params.id;
+        const estoqueExiste = await EstoqueDAO.listarEstoquePorId(bd, id_estoque);
+
+        if (!estoqueExiste) {
+            return res.status(404).json({ message: 'Não encontrado' });
         }
-        const messageDelete = {
-            success: 'Estoque deletado com sucesso.',
-            error: false
-        }
-        if (!EstoqueExiste) {
-            return res.status(404).json(message.success)
-        }
+
         try {
-            await EstoqueDAO.deletarEstoque(bd, id_estoque)
-            res.status(200).json(messageDelete.sucess)
+            await EstoqueDAO.deletarEstoque(bd, id_estoque);
+            res.status(200).json({ message: 'Deletado com sucesso' });
         } catch (erro) {
-            res.status(400).json({ error: erro.message });
+            res.status(400).json({ error: erro });
         }
     });
-}
+};
